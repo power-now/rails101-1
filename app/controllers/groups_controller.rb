9372d7c02ff3caaaa3a248e210e2c3b1781673af
lene marlin <ybs18s@163.com>
2016-12-30 21:40:43 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,9 +17,13 @@ class GroupsController < ApplicationController
 
     def create
       @group = Group.new(group_params)
-      @group.save
+
+      if @group.save
 
         redirect_to groups_path
+      else
+        render:new
+      end
     end
 
     def update
@@ -41,4 +45,4 @@ class GroupsController < ApplicationController
     def group_params
       params.require(:group).permit(:title, :description)
     end
-end
+  end
